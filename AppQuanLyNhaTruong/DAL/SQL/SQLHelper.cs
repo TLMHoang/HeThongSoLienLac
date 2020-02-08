06ff54209dc3a8b66a7d6b8b3856b0e7282a5efd
fsminhhoangfs <fsminhhoangfs@gmail.com>
2020-02-08 12:53:13 +0700

    update SQLHelper.cs

---
+++ AppQuanLyNhaTruong/DAL/SQL/SQLHelper.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DAL.SQL
+{
+    public class SQLHelper
+    {
+        private string connStr = @"StringNameDatabase";
+
+
+        public async Task<int> ExecuteNonQuery(string ProcName, params SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand(ProcName, con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        if (parameters != null)
+                        {
+                            cmd.Parameters.AddRange(parameters);
+                        }
+
+                        await con.OpenAsync();
+
+
+                        return await cmd.ExecuteNonQueryAsync();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi \n\n" + ex.ToString());
+                }
+                finally
+                {
+                    if (con.State == ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public async Task<DataTable> ExecuteQuery(string ProcName, params SqlParameter[] parameters)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand(ProcName, con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        if (parameters != null)
+                        {
+                            cmd.Parameters.AddRange(parameters);
+                        }
+
+                        await con.OpenAsync();
+
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            adapter.Fill(dt);
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi \n\n" + ex.ToString());
+                }
+                finally
+                {
+                    if (con.State == ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+                }
+            }
+
+            return dt;
+        }
+
+        public async Task<T> ExecuteScalar<T>(string ProcName, params SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand(ProcName, con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        if (parameters != null)
+                        {
+                            cmd.Parameters.AddRange(parameters);
+                        }
+
+                        await con.OpenAsync();
+                        var Val = await cmd.ExecuteScalarAsync();
+
+                        return (Val == null) ? default(T) : (T)Convert.ChangeType(Val, typeof(T));
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi \n\n" + ex.ToString());
+                }
+                finally
+                {
+                    if (con.State == ConnectionState.Open)
+                    {
+                        con.Close();
+                    }
+                }
+
+                return default(T);
+            }
+        }
+    }
+}
