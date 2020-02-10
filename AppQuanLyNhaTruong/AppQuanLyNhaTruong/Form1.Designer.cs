00b614f2428f78f1411cb95489030ddf18453520
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2020-02-10 10:31:42 +0700

    Update Form Login

--- AppQuanLyNhaTruong/AppQuanLyNhaTruong/Form1.Designer.cs
+++ AppQuanLyNhaTruong/AppQuanLyNhaTruong/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AppQuanLyNhaTruong
 {
-    partial class Form1
+    partial class frmLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.pnlUserName = new System.Windows.Forms.Panel();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.pnlPassWord = new System.Windows.Forms.Panel();
+            this.txtPassWord = new System.Windows.Forms.TextBox();
+            this.lblPassWord = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pnlLogin.SuspendLayout();
+            this.pnlUserName.SuspendLayout();
+            this.pnlPassWord.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Controls.Add(this.btnExit);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.pnlPassWord);
+            this.pnlLogin.Controls.Add(this.pnlUserName);
+            this.pnlLogin.Location = new System.Drawing.Point(12, 12);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(568, 246);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // pnlUserName
+            // 
+            this.pnlUserName.Controls.Add(this.txtUserName);
+            this.pnlUserName.Controls.Add(this.lblUserName);
+            this.pnlUserName.Location = new System.Drawing.Point(3, 3);
+            this.pnlUserName.Name = "pnlUserName";
+            this.pnlUserName.Size = new System.Drawing.Size(561, 63);
+            this.pnlUserName.TabIndex = 0;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.Location = new System.Drawing.Point(18, 19);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(120, 19);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "Tên Đăng Nhập";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(164, 16);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(374, 27);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // pnlPassWord
+            // 
+            this.pnlPassWord.Controls.Add(this.txtPassWord);
+            this.pnlPassWord.Controls.Add(this.lblPassWord);
+            this.pnlPassWord.Location = new System.Drawing.Point(3, 72);
+            this.pnlPassWord.Name = "pnlPassWord";
+            this.pnlPassWord.Size = new System.Drawing.Size(561, 63);
+            this.pnlPassWord.TabIndex = 1;
+            // 
+            // txtPassWord
+            // 
+            this.txtPassWord.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassWord.Location = new System.Drawing.Point(164, 16);
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.Size = new System.Drawing.Size(374, 27);
+            this.txtPassWord.TabIndex = 1;
+            this.txtPassWord.UseSystemPasswordChar = true;
+            // 
+            // lblPassWord
+            // 
+            this.lblPassWord.AutoSize = true;
+            this.lblPassWord.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassWord.Location = new System.Drawing.Point(18, 19);
+            this.lblPassWord.Name = "lblPassWord";
+            this.lblPassWord.Size = new System.Drawing.Size(81, 19);
+            this.lblPassWord.TabIndex = 0;
+            this.lblPassWord.Text = "Mật Khẩu";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(175, 162);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(180, 43);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Đăng Nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(361, 162);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(180, 43);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(590, 273);
+            this.Controls.Add(this.pnlLogin);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập";
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlUserName.ResumeLayout(false);
+            this.pnlUserName.PerformLayout();
+            this.pnlPassWord.ResumeLayout(false);
+            this.pnlPassWord.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Panel pnlPassWord;
+        private System.Windows.Forms.TextBox txtPassWord;
+        private System.Windows.Forms.Label lblPassWord;
+        private System.Windows.Forms.Panel pnlUserName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblUserName;
     }
 }
 
