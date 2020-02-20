9dc540e35eef8c41c9261a26892c596f5c2b673c
dmmlmm456 <dmmlmm456@gmail.com>
2020-02-20 12:00:04 +0700

    update hs

--- AppQuanLyNhaTruong/GUI/Program.cs
+++ AppQuanLyNhaTruong/GUI/Program.cs
@@ -21,7 +21,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmHocSinh());
         }
     }
 }
