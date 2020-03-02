8d759616f1bc96cef74fa031969a5718cefe7799
fsminhhoangfs <fsminhhoangfs@gmail.com>
2020-03-02 22:29:17 +0700

    add BAL.dll DAL.dll DTO.dll

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Controllers/ThongTinTaiKhoanController.cs
@@ -0,0 +1,18 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Controllers
+{
+    public class ThongTinTaiKhoanController : Controller
+    {
+        // GET: ThongTinTaiKhoan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
