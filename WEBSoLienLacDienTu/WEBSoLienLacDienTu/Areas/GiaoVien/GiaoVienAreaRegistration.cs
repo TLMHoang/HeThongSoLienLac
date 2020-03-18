f690d7dda0fa3dbc922b2e5337d8f9bbe10fcb49
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2020-03-18 18:40:56 +0700

    Update Area GiaoVien

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/GiaoVien/GiaoVienAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Areas.GiaoVien
+{
+    public class GiaoVienAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GiaoVien";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GiaoVien_default",
+                "GiaoVien/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
