3a0b46325bfeefb1c0b8dfdc07688cf31e06065b
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2020-03-20 11:15:45 +0700

    Update GiaoDien

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/Admin/Controllers/DiemDanhController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Areas.Admin.Controllers
+{
+    public class DiemDanhController : Controller
+    {
+        // GET: Admin/DiemDanh
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CupHoc()
+        {
+            return View();
+        }
+    }
+}
