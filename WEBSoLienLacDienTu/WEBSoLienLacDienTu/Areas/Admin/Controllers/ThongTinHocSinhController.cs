e3a2bc3f565bb94694066b797136152ad6bd4cbe
dmmlmm456 <dmmlmm456@gmail.com>
2020-03-26 15:51:45 +0700

    update 1 so giao dien

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/Admin/Controllers/ThongTinHocSinhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Areas.Admin.Controllers
+{
+    public class ThongTinHocSinhController : Controller
+    {
+        // GET: Admin/ThongTinHocSinh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
