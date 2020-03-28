af47f91f552d315f1d1ede384bc3f9da6e14a374
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2020-03-28 15:27:07 +0700

    Update GiaoDien

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/Admin/Controllers/PhanCongDayController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Areas.Admin.Controllers
+{
+    public class PhanCongDayController : Controller
+    {
+        // GET: Admin/PhanCongDay
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
