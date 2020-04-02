1cddc0f68be440879374db37910c34e395dd0186
dmmlmm456 <dmmlmm456@gmail.com>
2020-04-02 19:19:07 +0700

    update

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Controllers/TraCuuController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Controllers
+{
+    public class TraCuuController : Controller
+    {
+        // GET: TraCuu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GVBM()
+        {
+            return View();
+        }
+    }
+}
