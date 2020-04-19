246eb9bc162570674a1bca6838eeeba4bf8dd804
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2020-04-19 11:08:18 +0700

    Update BaoCao

--- WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/Admin/Models/GetNameClassModel.cs
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/Admin/Models/GetNameClassModel.cs
@@ -11,25 +11,29 @@ namespace WEBSoLienLacDienTu.Areas.Admin.Models
         public int ID { get; set; }
         public string TenKhoi { get; set; }
         public string TenLop { get; set; }
+        public string TenDayDu { get; set; }
 
         public GetNameClassModel()
         {
             ID = -1;
             TenKhoi = "";
             TenLop = "";
+            TenDayDu = "";
         }
 
-        public GetNameClassModel(int iD,string tenKhoi,string tenLop)
+        public GetNameClassModel(int iD,string tenKhoi,string tenLop,string tenDayDu)
         {
             ID = iD;
             TenKhoi = tenKhoi;
             TenLop = tenLop;
+            TenDayDu = tenDayDu;
         }
         public GetNameClassModel(DataRow dr)
         {
             ID = Convert.IsDBNull(dr["ID"]) ? -1 : Convert.ToInt32(dr["ID"]);
             TenKhoi = dr["TenKhoi"].ToString();
             TenLop = dr["TenLop"].ToString();
+            TenDayDu = dr["TenDayDu"].ToString();
         }
     }
 }
