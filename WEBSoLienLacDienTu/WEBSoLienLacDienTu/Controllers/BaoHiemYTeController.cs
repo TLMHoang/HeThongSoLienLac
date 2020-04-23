53685210805e4eda6b44926ec6e9f5c7a4ec2017
dmmlmm456 <dmmlmm456@gmail.com>
2020-04-23 21:01:31 +0700

    Update them

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Controllers/BaoHiemYTeController.cs
@@ -0,0 +1,29 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WEBSoLienLacDienTu.Home;
+using WEBSoLienLacDienTu.Models;
+
+namespace WEBSoLienLacDienTu.Controllers
+{
+    [SessionTimeout]
+    public class BaoHiemYTeController : Controller
+    {
+        // GET: BaoHiemYTe
+        
+        public async Task<ActionResult> Index()
+        {
+            List<LKPHvsHS> lst = new List<LKPHvsHS>();
+            foreach (DataRow dr in (await new ThongTinHSDAL().LayDT_ByIDTaiKhoan(TaiKhoanPhuHuynhController.TK.ID)).Rows)
+            {
+                lst.Add(new LKPHvsHS(dr));
+            }
+            return View(lst);
+        }
+    }
+}
---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Controllers/TongKetCuoiKyController.cs
@@ -0,0 +1,29 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WEBSoLienLacDienTu.Home;
+using WEBSoLienLacDienTu.Models;
+
+namespace WEBSoLienLacDienTu.Controllers
+{
+    [SessionTimeout]
+    public class TongKetCuoiKyController : Controller
+    {
+        // GET: TongKetCuoiKy
+        
+        public async Task<ActionResult> Index()
+        {
+            List<LKPHvsHS> lst = new List<LKPHvsHS>();
+            foreach (DataRow dr in (await new ThongTinHSDAL().LayDT_ByIDTaiKhoan(TaiKhoanPhuHuynhController.TK.ID)).Rows)
+            {
+                lst.Add(new LKPHvsHS(dr));
+            }
+            return View(lst);
+        }
+    }
+}
