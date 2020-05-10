7275c33195e8b53e38f42c36bc5cf4489944fc0b
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2020-05-10 11:57:35 +0700

    Update Code BHYT

---
+++ WEBSoLienLacDienTu/DAL/BHYT_DAL.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class BHYT_DAL : SQL.SQLHelper,CInterface<BHYT>
+    {
+        public async Task<DataTable> LayDT()
+        {
+            return await ExecuteQuery("SelectBHYT",
+                new SqlParameter("@IDHocSinh", SqlDbType.Int) { Value = -1 }
+            );
+        }
+
+        public async Task<DataTable> LayDT(int ID)
+        {
+            return await ExecuteQuery("SelectBHYT",
+                new SqlParameter("@IDHocSinh", SqlDbType.Int) { Value = ID }
+            );
+        }
+
+        public async Task<List<BHYT>> LayLst()
+        {
+            List<BHYT> lst = new List<BHYT>();
+            foreach (DataRow dr in (await LayDT()).Rows)
+            {
+                lst.Add(new BHYT(dr));
+            }
+            return lst;
+        }
+
+        public async Task<int> CapNhap(BHYT obj)
+        {
+            return await ExecuteNonQuery("UpdateBHYT",
+                new SqlParameter("@IDHocSinh", SqlDbType.Int) { Value = obj.IDHocSinh},
+                new SqlParameter("@DangKy", SqlDbType.Bit) { Value = obj.DangKy },
+                new SqlParameter("@BHQD", SqlDbType.Bit) { Value = obj.BHQD }
+            );
+        }
+
+        public async Task<int> Xoa(int ID)
+        {
+            return await ExecuteNonQuery("DeleteBHYT", new SqlParameter("@IDHocSinh", SqlDbType.Int) { Value = ID });
+        }
+
+        public async Task<int> Them(BHYT obj)
+        {
+            return await ExecuteNonQuery("InsertBHYT",
+                new SqlParameter("@IDHocSinh", SqlDbType.Int) { Value = obj.IDHocSinh },
+                new SqlParameter("@DangKy", SqlDbType.Bit) { Value = obj.DangKy },
+                new SqlParameter("@BHQD", SqlDbType.Bit) { Value = obj.BHQD }
+            );
+        }
+        public async Task<DataTable> CheckDateBhyt()
+        {
+            return await ExecuteQuery("W_SelectFunctionOrder_BHYT");
+        }
+    }
+}
---
+++ WEBSoLienLacDienTu/DTO/FunctionOrder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class FunctionOrder
+    {
+        public int ID { get; set; }
+        public string NameFunc { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public FunctionOrder()
+        {
+            ID = -1;
+            NameFunc = "";
+            StartDate = DateTime.Now;
+            EndDate = DateTime.Now;
+        }
+
+        public FunctionOrder(int id, string nameFunc, DateTime startDate, DateTime endDate)
+        {
+            ID = id;
+            NameFunc = nameFunc;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public FunctionOrder(DataRow dr)
+        {
+            ID = Convert.IsDBNull(dr["ID"]) ? -1 : Convert.ToInt32(dr["ID"]);
+            NameFunc = dr["NameFunc"].ToString();
+            StartDate = Convert.ToDateTime(dr["StartDate"]);
+            EndDate = Convert.ToDateTime(dr["EndDate"]);
+        }
+    }
+}
