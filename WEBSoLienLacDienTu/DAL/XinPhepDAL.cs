e6079bb150dfea4188643b500bc482cd46571449
dmmlmm456 <dmmlmm456@gmail.com>
2020-05-24 00:02:11 +0700

    fix bug xinp

--- WEBSoLienLacDienTu/DAL/XinPhepDAL.cs
+++ WEBSoLienLacDienTu/DAL/XinPhepDAL.cs
@@ -126,7 +126,7 @@ namespace DAL
         public async Task<DataTable> LayDT_ID(int ID)
         {
             return await ExecuteQuery(
-                "SelectXinPhepTheoIDHSV2",               
+                "SelectXinPhep2",               
                 new SqlParameter("@ID", SqlDbType.Int) { Value = ID }
             );
         }
