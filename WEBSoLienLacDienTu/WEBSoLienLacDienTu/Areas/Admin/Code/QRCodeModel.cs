29389c21bcaf3231195314d62572de7d725dae26
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2021-03-21 21:02:20 +0700

    update code

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/Admin/Code/QRCodeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WEBSoLienLacDienTu.Areas.Admin.Code
+{
+    public class QRCodeModel
+    {
+        [Display(Name = "QRCode Text")]
+        public string QRCodeText { get; set; }
+        [Display(Name = "QRCode Image")]
+        public string QRCodeImagePath { get; set; }
+    }
+}
--- WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/Admin/Controllers/QRController.cs
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/Admin/Controllers/QRController.cs
@@ -1,10 +1,15 @@
 ﻿using DAL;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using WEBSoLienLacDienTu.Areas.Admin.Code;
+using ZXing;
 
 namespace WEBSoLienLacDienTu.Areas.Admin.Controllers
 {
@@ -16,9 +21,78 @@ namespace WEBSoLienLacDienTu.Areas.Admin.Controllers
             await LoadListKhoi();
             return View();
         }
+        public async Task<ActionResult> IndexQR()
+        {
+            
+            return View();
+        }
         public async Task LoadListKhoi()
         {
             ViewBag.LstKhoi = new SelectList(await new KhoiDAL().LayLst(), "ID", "TenKhoi");
+        }
+
+        [HttpPost]
+        public ActionResult Generate(QRCodeModel qrcode)
+        {
+            try
+            {
+                qrcode.QRCodeImagePath = GenerateQRCode(qrcode.QRCodeText);
+                ViewBag.Message = "QR Code Created successfully";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return View("IndexQR", qrcode);
+        }
+
+        public ActionResult Read()
+        {
+            return View(ReadQRCode());
+        }
+
+        private QRCodeModel ReadQRCode()
+        {
+            QRCodeModel barcodeModel = new QRCodeModel();
+            string barcodeText = "";
+            string imagePath = "~/Assets/Img/QrCode.jpg";
+            string barcodePath = Server.MapPath(imagePath);
+            var barcodeReader = new BarcodeReader();
+
+            var result = barcodeReader.Decode(new Bitmap(barcodePath));
+            if (result != null)
+            {
+                barcodeText = result.Text;
+            }
+            return new QRCodeModel() { QRCodeText = barcodeText, QRCodeImagePath = imagePath };
+        }
+
+        private string GenerateQRCode(string qrcodeText)
+        {
+            string folderPath = "~/Assets/Img";
+            string imagePath = "~/Assets/Img/QrCode.jpg";
+            // If the directory doesn't exist then create it.
+            if (!Directory.Exists(Server.MapPath(folderPath)))
+            {
+                Directory.CreateDirectory(Server.MapPath(folderPath));
+            }
+
+            var barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+            var result = barcodeWriter.Write(qrcodeText);
+
+            string barcodePath = Server.MapPath(imagePath);
+            var barcodeBitmap = new Bitmap(result);
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(barcodePath, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    barcodeBitmap.Save(memory, ImageFormat.Jpeg);
+                    byte[] bytes = memory.ToArray();
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            }
+            return imagePath;
         }
     }
 }
