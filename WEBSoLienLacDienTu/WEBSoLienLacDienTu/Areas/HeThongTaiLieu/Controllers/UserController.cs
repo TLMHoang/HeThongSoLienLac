05218384c2e63b8d133889dbc9db7f9daef66c1a
vuhoangphong <73226382+vuhoangphong@users.noreply.github.com>
2021-04-10 18:19:45 +0700

    Update area TaiLieu And Create Login Page TaiLieu

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/HeThongTaiLieu/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Areas.HeThongTaiLieu.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: HeThongTaiLieu/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login_TaiLieuONL()
+        {
+            return View();
+        }
+    }
+}
---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/HeThongTaiLieu/HeThongTaiLieuAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Areas.HeThongTaiLieu
+{
+    public class HeThongTaiLieuAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HeThongTaiLieu";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HeThongTaiLieu_default",
+                "HeThongTaiLieu/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
