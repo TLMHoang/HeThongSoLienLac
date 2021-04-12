614a606342f9bdb83d51bb8807c1482e39f70daa
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2021-04-12 22:49:32 +0700

    Update Login

--- WEBSoLienLacDienTu/WEBSoLienLacDienTu/App_Start/RouteConfig.cs
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace WEBSoLienLacDienTu
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+           
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
