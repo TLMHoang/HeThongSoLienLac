aa01a690f0c9fe18329d39def47538462da4452a
vhp159357 <52731971+vhp159357@users.noreply.github.com>
2021-05-10 18:25:38 +0700

    Create Upload GV

---
+++ WEBSoLienLacDienTu/WEBSoLienLacDienTu/Areas/GiaoVien/Controllers/HeThongTaiLieu_GVController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSoLienLacDienTu.Areas.GiaoVien.Controllers
+{
+    public class HeThongTaiLieu_GVController : Controller
+    {
+        // GET: GiaoVien/HeThongTaiLieu_GV
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Quiz()
+        {
+            return View();
+        }
+        public ActionResult Documents()
+        {
+            return View();
+        }
+    }
+}
